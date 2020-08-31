a3b0b879057832023e60f289cb4ed16856a5fa64
Roman Belov <belov.rom-77@yandex.ru>
2020-08-31 07:27:56 +0300

    Создает и настраивает базовые шаблоны для страниц

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,20 @@
 "use strict";
 
+const path = require(`path`);
 const express = require(`express`);
 const mainRouter = require(`./routes/main-routes`);
 const registerRouter = require(`./routes/register-routes`);
-const loginRouter = require(`./routes/login`);
+const loginRouter = require(`./routes/login-routes`);
 const myRouter = require(`./routes/my-routes`);
 const offersRouter = require(`./routes/offers-routes`);
 const searchRouter = require(`./routes/search-routes`);
-const {DEFAULT_EXPRESS_PORT} = require(`../constants`);
+const {DEFAULT_EXPRESS_PORT, PUBLIC_DIR, TEMPLATES_DIR} = require(`../constants`);
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRouter);
 app.use(`/register`, registerRouter);
