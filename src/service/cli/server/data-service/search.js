1942bdb773327fb00c761d9771712683a4263dfc
Belov Roman <rnbelov96@gmail.com>
2020-11-26 07:43:35 +0300

    Выносит опции запроса в отдельную функцию

--- src/service/cli/server/data-service/search.js
+++ src/service/cli/server/data-service/search.js
@@ -1,37 +1,13 @@
 "use strict";
 
+const {getSequelizeQueryOptions} = require(`../../../../constants`);
 const {db} = require(`../db/db`);
 
 class SearchService {
   async findAll(searchText) {
-    const offers = await db.Offer.findAll({
-      attributes: {exclude: [`userId`, `typeId`]},
-      include: [
-        {
-          model: db.User,
-          as: `owner`,
-          attributes: [`id`, `firstName`, `lastName`, `email`],
-        },
-        {model: db.OfferType, as: `offerType`},
-        {
-          model: db.Comment,
-          as: `comments`,
-          attributes: {exclude: [`userId`, `offerId`]},
-          include: {
-            model: db.User,
-            as: `user`,
-            attributes: [`id`, `firstName`, `lastName`, `email`],
-          },
-        },
-        {
-          model: db.Category,
-          as: `categories`,
-          through: {
-            attributes: [],
-          },
-        },
-      ],
-    });
+    const offers = await db.Offer.findAll(
+        getSequelizeQueryOptions(`Offer`, db)
+    );
 
     return offers.filter((offer) => {
       return offer.title.toLowerCase().includes(searchText.toLowerCase());
